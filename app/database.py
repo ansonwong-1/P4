721f965101b1aceb0830b6ab145e2ca313ebe5cf
Brian Chen <bcchen52@gmail.com>
2023-05-08 09:23:48 -0400

    s

---
+++ app/database.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+DB_FILE = "database.db"
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+
+def setup_tables():
+    c = db.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS flights (f_id INTEGER PRIMARY KEY, deparature TEXT, arrival TEXT)")
+    db.commit()
+    c.close()
+
+def add_flights():
+    c = db.cursor()
+    c.execute("INSERT INTO flights VALUES(?, ?, ?)", (0,"123","123") )
+    db.commit()
+    c.close()
---
+++ app/db_test.py
@@ -0,0 +1,4 @@
+import database as db
+
+db.setup_tables()
+
