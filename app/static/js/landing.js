837344c8d82f6f64e74a3b44de43011b8c6306a9
Anson <awong30@stuy.edu>
2023-05-10 11:01:42 -0400

    empty map centered at school

---
+++ app/static/js/landing.js
@@ -0,0 +1,24 @@
+var map = L.map('map', {
+    center: [40, 285],
+    zoom: 5
+});
+
+/*
+diff links for tiles but still quite ugly
+["http://a.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png","http://b.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png","http://c.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png"]
+["http://a.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://b.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://d.tile.stamen.com/watercolor/{z}/{x}/{y}.png"]
+["http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://c.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"]
+*/
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+/*
+longitude for ny didnt match what i got from google search so copied this from leaflet
+function onMapClick(e) {
+    console.log("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
+*/
