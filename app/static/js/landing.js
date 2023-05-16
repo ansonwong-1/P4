00b881e28a2827f01b6627e3b75c8359e376ff50
mahirriki1 <mriki30@stuy.edu>
2023-05-15 22:08:14 -0400

    made new branch and edits

--- app/static/js/landing.js
+++ app/static/js/landing.js
@@ -1,7 +1,7 @@
 // creation of map
-const map = L.map('map', {
-    center: [50,0],
-    zoom: 2
+const map = L.map("map", {
+  center: [50, 0],
+  zoom: 2,
 });
 
 /*
@@ -10,48 +10,45 @@ diff links for tiles but still quite ugly
 ["http://a.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://b.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://d.tile.stamen.com/watercolor/{z}/{x}/{y}.png"]
 ["http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://c.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"]
 */
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
 // markers for planes
 const planeIcon = L.icon({
-    iconUrl: 'static/img/plane.png', // i found a sample plane pic online
-    iconSize: [45, 45],
-    iconAnchor: [15, 15],
-    //popupAnchor: [0, -15]
+  iconUrl: "static/img/plane.png", // i found a sample plane pic online
+  iconSize: [45, 45],
+  iconAnchor: [15, 15],
+  //popupAnchor: [0, -15]
 });
 
-// real-time data stuff -- will come from database hopefully
+// real-time data stuff -- adds markers to maps too
 function getPlanes() {
-    // $.ajax({
-    //     url: '/get-planes', 
-    //     type: 'GET',
-    //     success: function(data) {
-    //         displayPlanes(data);
-    //     },
-    //     error: function(error) {
-    //         console.log(error);
-    //     }
-    // });
-    var planes = [
-        //this is not statue of liberty idk how lng works in leaflet :/
-        { id: 1, name: 'Plane 1', lat: 40.6892, lng: 74.0445 }, // statue of liberty
-        { id: 2, name: 'Plane 2', lat: 40.7128, lng: -74.0060 },
-        { id: 3, name: 'Plane 3', lat: 51.5074, lng: -0.1278 }
-      ];
-    displayPlanes(planes);
+  axios
+    .get("/planes")
+    .then(function (response) {
+      var planes = response.data;
+      map.eachLayer(function (layer) {
+        if (layer instanceof L.Marker) {
+          map.removeLayer(layer);
+        }
+      });
+      displayPlanes(planes);
+      planes.forEach(function (plane) {
+        var lat = plane.lat;
+        var longi = plane.longi;
+        if (lat && longi) {
+          L.marker([lat, longi], { icon: planeIcon }).addTo(map);
+        }
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
-
-function displayPlanes(planes) {
-    for (let i = 0; i < planes.length; i++) {
-        const plane = planes[i];
-        const marker = L.marker([plane.lat, plane.lng], { icon: planeIcon }).addTo(map);
-        marker.bindPopup(plane.name).openPopup(); //can put wtv flight info in () not need another page
-    }
-}
-getPlanes()
+getPlanes();
 /*
 longitude for ny didnt match what i got from google search so copied this from leaflet
 function onMapClick(e) {
